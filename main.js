8fde061dc004e316a1cd3d4fee16357eee5e5dba
Zac McChesney <imzacm@gmail.com>
2016-03-13 10:05:14 +0000

    Main shell and functions
    current functions: command | runs specified functions, writeToShell |
    self-explanatory, echo | also self-explanatory, addlib | imports a
    library, info | outputs info about the shell, commList | returns an
    array of commands, help | outputs the commands, usage and a description.

---
+++ main.js
@@ -0,0 +1,76 @@
+function command(comm) {
+  var commSplit = comm.split(" ");
+  var commargs = ["args"];
+  for (i = 1; i < commSplit.length; i++) {
+    commargs.push(commSplit[i]);
+  }
+  var commands = commList();
+  if (commands.indexOf(commSplit[0]) != -1) {
+    window[commSplit[0]](commargs);
+  }
+  else {
+    writeToShell('command: "' + comm + '" does not exist')
+  }
+}
+
+function writeToShell(str) {
+  if (str.constructor === Array) {
+    $("#shell").append('<b class="user">user:</b> ' + str[0] + "<br>");
+    console.log("user: " + str[0]);
+    for (i = 1; i < str.length; i++) {
+      $("#shell").append('<b class="user">&nbsp &nbsp &nbsp &nbsp</b> ' + str[i] + "<br>");
+      console.log(str[i]);
+    }
+  }
+  else {
+    $("#shell").append('<b class="user">user:</b> ' + str + "<br>");
+    console.log("user: " + str);
+  }
+}
+
+function echo(str) {
+  if (str.length == 1) {
+    writeToShell('missing argument, run "help" for usage');
+  }
+  else {
+    if (str.constructor === Array) {
+      console.log("echo: ");
+      $("#shell").append("echo: <br>");
+      for (i = 1; i < str.length; i++) {
+        $("#shell").append('<b class="user">&nbsp &nbsp &nbsp &nbsp</b> ' + str[i] + " ");
+        console.log(str[i]);
+      }
+      $("#shell").append("<br>");
+      console.log("end echo;");
+    }
+  }
+}
+
+function addlib(lib) {
+  $.getScript(lib, function() {
+    console.log('Loaded script: ' + lib);
+  });
+}
+
+function info() {
+  var infoArray = ["Shell info"];
+  infoArray.push("Author | Zac McChesney");
+  infoArray.push("Version | v1.0");
+  writeToShell(infoArray);
+  $("#shell").append('<b class="user">&nbsp &nbsp &nbsp &nbsp</b> ' + 'Website | <a href="http://zacmcchesney.com" target="_blank">zacmcchesney.com<a>' + "<br>");
+}
+
+function commList() {
+  var commands = ["help"];
+  commands.push("echo");
+  commands.push("info");
+  return commands;
+}
+
+function help() {
+  var helpArray = ["help command"];
+  helpArray.push("help | displays a list of commands with descriptions");
+  helpArray.push('echo "echo text" | outputs the "echo text"');
+  helpArray.push("info | displays information about the shell");
+  writeToShell(helpArray);
+}
